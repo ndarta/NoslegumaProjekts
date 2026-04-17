922f67ef68a2325b75d8159fc720d7dcf0358ffc
ndarta <darta.novika@gmail.com>
2026-04-17 21:37:48 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 function radio(){
     if (document.getElementById("ja").checked) {
         document.getElementById("vai").innerHTML =
-        `Vari doties uz <a href="spele.html" class="saite spele">Spēli</a>`;
+        `Vari doties uz <a href="spele.html" class="spele">Spēli</a>`;
         
     } else if (document.getElementById("ne").checked) {
         document.getElementById("vai").innerHTML =
-        `Izlasi vēlreiz teoriju un tad dodies uz <a href="tests.html" class="saite tests">testu</a>!`;
+        `Izlasi vēlreiz teoriju un tad dodies uz <a href="tests.html" class="tests">testu</a>!`;
     }
 }
