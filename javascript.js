d5594c184ca2b864f61c40b8148e538762ee7694
ndarta <darta.novika@gmail.com>
2026-04-21 12:08:13 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function radio(){
     if(document.getElementById("ja").checked) {
        document.getElementById("vai").innerHTML =  `Vari doties uz Spēli`
     } else if ( document.getElementById("ne").checked ) {
-       document.getElementById("vai").innerHTML =  `Izlasi vēlreiz teoriju1 un tad vari doties uz spēli`
+       document.getElementById("vai").innerHTML =  `Žēl, izlasi vēlreiz teoriju un izpildi testu`
         
     }
 }
