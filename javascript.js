d76700dfda2f4d9e564f92373a626e1e5b9f6206
ggg-08 <gretagirgensone@gmail.com>
2026-04-21 12:19:16 +0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 function radio(){
     if(document.getElementById("ja").checked) {
-       document.getElementById("vai").innerHTML =  `Vari doties uz Spēli`
+       document.getElementById("vai").innerHTML =  'Vari doties uz <a href="spele.html">Spēle</a>'
     } else if ( document.getElementById("ne").checked ) {
-       document.getElementById("vai").innerHTML =  `Žēl, izlasi vēlreiz teoriju un izpildi testu`
+       document.getElementById("vai").innerHTML =  'Izlasi vēlreiz teoriju vai izpildi <a href="index2.html">Testu</a>'
         
     }
-}
+    }
+   
+
