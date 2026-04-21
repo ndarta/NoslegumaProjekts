8a1b1ee61e6e3a84f5fb1c068df485d842c138b3
ndarta <darta.novika@gmail.com>
2026-04-21 12:30:15 +0300

    Delete javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-function radio(){
-    if(document.getElementById("ja").checked) {
-       document.getElementById("vai").innerHTML =  'Vari doties uz <a href="spele.html">Spēle</a>'
-    } else if ( document.getElementById("ne").checked ) {
-       document.getElementById("vai").innerHTML =  'Izlasi vēlreiz teoriju vai izpildi <a href="index2.html">Testu</a>'
-        
-    }
-    }
-   
